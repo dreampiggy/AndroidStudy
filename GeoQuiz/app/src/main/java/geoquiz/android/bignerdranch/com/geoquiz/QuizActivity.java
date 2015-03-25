8a717cd6bf163f73de8f14da8e542c8ce4a9a995
lizhuoli <lizhuoli1126@126.com>
2015-03-25 20:58:49 +0800

    1、I'm boring -.-
    跟着教程走的一个无聊App，答题应用

--- GeoQuiz/app/src/main/java/geoquiz/android/bignerdranch/com/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/geoquiz/android/bignerdranch/com/geoquiz/QuizActivity.java
@@ -4,14 +4,117 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class QuizActivity extends ActionBarActivity {
+
+    private ImageButton mAuthorButton;
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mLastButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_1,false),
+            new TrueFalse(R.string.question_2,true),
+            new TrueFalse(R.string.question_3,false),
+            new TrueFalse(R.string.question_4,false),
+            new TrueFalse(R.string.question_5,true)
+    };
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+        int messageResID = 0;
+
+        if(userPressedTrue == answerIsTrue){
+            messageResID = R.string.correct_toast;
+        }
+        else{
+            messageResID = R.string.incorrect_toast;
+        }
+        Toast.makeText(this,messageResID,Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        mAuthorButton = (ImageButton)findViewById(R.id.author_picture);
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mLastButton = (Button)findViewById(R.id.last_button);
+        mNextButton = (Button)findViewById(R.id.next_button);
+
+        mAuthorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,R.string.author_name,Toast.LENGTH_SHORT).show();
+            }
+        });
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+        mLastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCurrentIndex <= 0){
+                    Toast.makeText(QuizActivity.this,R.string.begin_text,Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                    updateQuestion();
+                }
+            }
+        });
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCurrentIndex >= 4){
+                    Toast.makeText(QuizActivity.this,R.string.final_text,Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                    updateQuestion();
+                }
+            }
+        });
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCurrentIndex >= 4){
+                    ;
+                }
+                else{
+                    mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                    updateQuestion();
+                }
+            }
+        });
+
+        updateQuestion();
     }
 
 
